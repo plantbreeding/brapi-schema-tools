963b451317a7abcd28a82c5979f13b71a5315aed
Guy Davenport <guy.davenport@partners.basf.com>
2024-06-12 21:00:25 +1200

    remove parent1 and parent2 objects, updated StringUtils

---
+++ java/core/src/test/java/org/brapi/schematools/core/utils/StringUtilsTest.java
@@ -0,0 +1,43 @@
+package org.brapi.schematools.core.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilsTest {
+
+    @Test
+    void toSingular() {
+        assertEquals("DataMatrix", StringUtils.toSingular("DataMatrices")) ;
+        assertEquals("Analysis", StringUtils.toSingular("Analysis")) ;
+    }
+
+    @Test
+    void toPlural() {
+        assertEquals("DataMatrices", StringUtils.toPlural("DataMatrix")) ;
+    }
+
+    @Test
+    void makeValidName() {
+    }
+
+    @Test
+    void toSentenceCase() {
+    }
+
+    @Test
+    void toParameterCase() {
+    }
+
+    @Test
+    void startsWithLowerCase() {
+    }
+
+    @Test
+    void startsWithUpperCase() {
+    }
+
+    @Test
+    void replace() {
+    }
+}
